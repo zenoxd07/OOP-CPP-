c77e1c4499535326348037d1dd5d10af9babcbae
Zeno <zenoxd07@gmail.com>
2026-08-03 17:28:30 +0545

    Pointer object

---
+++ 3pointerobject.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+class Student{
+        public:
+        int age;
+        string name;
+        void display(){
+            cout<<name<<endl<<age<<endl;
+
+        }
+
+
+};
+
+
+
+
+
+
+int main(){
+
+        Student s1;
+        s1.name="zeno";
+        s1.age=20;
+        Student *ptr = &s1;
+        ptr->display();
+        return 0;
+
+
+}
