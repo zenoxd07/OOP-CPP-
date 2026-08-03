2373ec9c4a98c29fe8ccefcbbe679e65964c8799
Zeno <zenoxd07@gmail.com>
2026-08-03 18:28:31 +0545

    Constructor and array

---
+++ 4constructorarray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Student{ 
+
+    public:
+  
+    Student(){
+        
+        cout<<"Constructor called "<<endl;
+      
+    }
+};
+
+int main(){
+        Student s[3];
+        
+}
